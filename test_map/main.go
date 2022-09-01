9abe693b5bd2244ec03fc733baa9814aeb035c9c
hexiaohu <272287207@qq.com>
2022-09-01 22:32:36 +0800

    init

--- test_map/main.go
+++ test_map/main.go
@@ -5,6 +5,12 @@ import (
 	"fmt"
 )
 
+type proper struct {
+	Name   string `json:"name"`
+	Age    int    `json:"age"`
+	Height float32
+}
+
 func main() {
 	stu_map := make(map[string]int)
 	stu_map["zhangsan"] = 8
@@ -26,5 +32,12 @@ func main() {
 		fmt.Println("error:", err2)
 	}
 	fmt.Println("json:", abc)
-
+	p := proper{Name: "张三", Age: 15, Height: 19.8}
+	fmt.Println(p)
+	res2, err3 := json.MarshalIndent(p, "", " ")
+	if err3 != nil {
+		fmt.Println("error", err3)
+		return
+	}
+	fmt.Println("res:", string(res2))
 }
