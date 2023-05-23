fc46c96d9d3889129734151fdd67888496d26f98
myNextWeekend <jinhu007@outlook.com>
2023-05-23 20:02:28 +0800

    学习flag

--- study/study_flag/flag.go
+++ study/study_flag/flag.go
@@ -3,17 +3,25 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
 var age int
 
 func main() {
 	//方式一
-	name := flag.String("name", "张三", "please input your name")
+	name := flag.String("name", "", "please input your name")
 	//方式二
 	flag.IntVar(&age, "age", 18, "please input your age")
 
 	flag.Parse()
+	// 如果名字为空，就报错
+	if *name == "" {
+		fmt.Println("Please enter the correct value ！")
+		flag.Usage()
+		os.Exit(1)
+	}
+
 	fmt.Println(*name)
 	fmt.Println(age)
 
