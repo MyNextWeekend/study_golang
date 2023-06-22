4603990315f7cbfed80ad2780c4afed5ba1feec2
myNextWeekend <jinhu007@outlook.com>
2023-06-22 11:59:49 +0800

    学习

--- example/limiter/limiter_test.go
+++ example/limiter/limiter_test.go
@@ -7,27 +7,15 @@ import (
 )
 
 func TestLimiter(t *testing.T) {
-	limiter := NewLimiter(2*time.Second, 2)
-	ticker := time.NewTicker(time.Second)
-	defer ticker.Stop()
-	done := make(chan bool)
-	go func() {
-		time.Sleep(10 * time.Second)
-		done <- true
-	}()
-	for {
-		select {
-		case <-done:
-			fmt.Println("Done!")
-			return
-		case t := <-ticker.C:
-			fmt.Println("Current time: ", t.Format("2006-01-02 15:04:05"))
-			if limiter.Allow() {
-				fmt.Println("allow requests....")
-			} else {
-				fmt.Println("not allow request!!!!")
-			}
+	limiter := NewLimiter(2*time.Second, 2) // 每2秒生成1个令牌，桶容量为2
+
+	for i := 1; i <= 10; i++ {
+		if limiter.Allow() {
+			fmt.Printf("Request %d passed\n", i)
+		} else {
+			fmt.Printf("Request %d blocked\n", i)
 		}
+		time.Sleep(500 * time.Millisecond) // 模拟请求间隔
 	}
 
 }
